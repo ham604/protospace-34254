9f0e89d869a66c7ab5aefc61b2a7e0f25e977632
ham604 <78540199+ham604@users.noreply.github.com>
2021-04-03 16:51:39 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,57 @@
+class PrototypesController < ApplicationController
+  #以下はauthenticate_user!を用いて制限している
+  before_action :authenticate_user!,only: [:update,:edit,:destroy,:new]
+  
+  def index
+    @prototypes = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+    #以下は投稿者以外がeditにアクセスしたらトップページにリダイレクトする記述
+    unless current_user.id == @prototype.user.id 
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+end
+
